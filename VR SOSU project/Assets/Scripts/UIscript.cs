8aac598be303bcb94639ad4d3a0a664f4cec0f42
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-03-27 12:43:43 +0100

    Working ingame menu
    ingame menu for leaving the level

---
+++ VR SOSU project/Assets/Scripts/UIscript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIscript : MonoBehaviour
+{
+    
+    public void Leave()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
