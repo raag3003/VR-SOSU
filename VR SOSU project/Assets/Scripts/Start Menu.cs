320c6a91c63d3435d85cae667258296554bc0140
dast0012 <dast0012@hotmail.com>
2025-04-13 14:04:41 +0200

    Levels menu
    Tilføjet level-menu til startmenuen. Når spilleren trykker på 'Start'-knappen, åbnes level-menuen, hvor de kan vælge mellem forskellige baner. I øjeblikket er der kun to baner tilgængelige.

--- VR SOSU project/Assets/Scripts/Start Menu.cs
+++ VR SOSU project/Assets/Scripts/Start Menu.cs
@@ -3,14 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
-    public void StartGame()
+    /*public void StartGame()
     {
         SceneManager.LoadScene(1);
-    }
+    }*/
 
     public void Quit()
     {
         Application.Quit();
         Debug.Log("Quitting the game");
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
     }
 }
