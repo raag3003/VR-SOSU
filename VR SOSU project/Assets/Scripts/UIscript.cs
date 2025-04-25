144104a21ad6ff692d712dc823389fef7fd7b180
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-04-25 12:19:29 +0200

    created final start menu
    Tried to freeze in place but came to the conclusion that a environment start menu would be the best

--- VR SOSU project/Assets/Scripts/UIscript.cs
+++ VR SOSU project/Assets/Scripts/UIscript.cs
@@ -4,8 +4,21 @@ using UnityEngine.SceneManagement;
 public class UIscript : MonoBehaviour
 {
     
+    // Leaves back to the main menu
     public void Leave()
     {
         SceneManager.LoadScene(0);
+    }
+
+    // quits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Loads the first scene (level 1)
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
