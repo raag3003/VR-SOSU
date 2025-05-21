19bd71c257efd6f72dcba95f54de79116484b3ee
Cheddarboi <125018416+Cheddarboi@users.noreply.github.com>
2025-05-21 11:16:39 +0200

    Voice 3

--- VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
+++ VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Oculus.Voice;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class VoiceScript : MonoBehaviour
 {
     public AppVoiceExperience voiceExperience;
+    public UnityEvent onPressed, onReleased, onCorrectAnswer, onIncorrectAnswer;
+    private bool isPressed;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +22,31 @@ public class VoiceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetUp(OVRInput.Button.One) || Input.GetKey(KeyCode.K))
+        if (isPressed || Input.GetKey(KeyCode.K))
         {
             voiceExperience.Activate();
         }
+
+
+
     }
+
+    public void VoiceActivate()
+    {
+        voiceExperience.Activate();
+    }
+
+
+
+    /*public void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        isPressed = true;
+
+    }
+
+    public void OnSelectExited(SelectExitEventArgs args)
+    {
+        isPressed = false;
+
+    }*/
 }
