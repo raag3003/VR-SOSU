c42404483b32781606952fdeabf67ca9cac179f9
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-05-22 04:04:31 +0200

    Revert "Voice 3"
    This reverts commit 19bd71c257efd6f72dcba95f54de79116484b3ee.

--- VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
+++ VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
@@ -2,16 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Oculus.Voice;
-using UnityEngine.Events;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class VoiceScript : MonoBehaviour
 {
     public AppVoiceExperience voiceExperience;
-    public UnityEvent onPressed, onReleased, onCorrectAnswer, onIncorrectAnswer;
-    private bool isPressed;
-
 
     // Start is called before the first frame update
     void Start()
@@ -22,31 +15,9 @@ public class VoiceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isPressed || Input.GetKey(KeyCode.K))
+        if (OVRInput.GetUp(OVRInput.Button.One) || Input.GetKey(KeyCode.K))
         {
             voiceExperience.Activate();
         }
-
-
-
     }
-
-    public void VoiceActivate()
-    {
-        voiceExperience.Activate();
-    }
-
-
-
-    /*public void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        isPressed = true;
-
-    }
-
-    public void OnSelectExited(SelectExitEventArgs args)
-    {
-        isPressed = false;
-
-    }*/
 }
