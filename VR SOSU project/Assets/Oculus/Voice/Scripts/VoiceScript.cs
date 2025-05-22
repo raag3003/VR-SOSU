f8b1245f8f91f2411f8a4907f1a5fea20d4f0c0e
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-05-22 04:03:46 +0200

    Revert "OVRManager"
    This reverts commit fba3f6b66063cd3b863b833f015ddc27498485df.

--- VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
+++ VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
@@ -22,7 +22,7 @@ public class VoiceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetUp(OVRInput.Button.One) || Input.GetKey(KeyCode.K))
+        if (isPressed || Input.GetKey(KeyCode.K))
         {
             voiceExperience.Activate();
         }
