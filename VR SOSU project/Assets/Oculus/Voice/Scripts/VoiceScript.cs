e8d7a754a2b0003152c1b180c9b41c99c4ad57e4
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-05-22 03:37:18 +0200

    Revert "Voice test"
    This reverts commit 1b0784eaea7678be68460466467c8894fa958487.

--- VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
+++ VR SOSU project/Assets/Oculus/Voice/Scripts/VoiceScript.cs
@@ -11,8 +11,7 @@ public class VoiceScript : MonoBehaviour
     public AppVoiceExperience voiceExperience;
     public UnityEvent onPressed, onReleased, onCorrectAnswer, onIncorrectAnswer;
     private bool isPressed;
-    public string Korrekt = "Så fik du vasket ansigtet";
-    public string Forkert = "Det vat godt gået";
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,21 +22,19 @@ public class VoiceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (OVRInput.GetUp(OVRInput.Button.One) || Input.GetKey(KeyCode.K))
+        if (OVRInput.GetUp(OVRInput.Button.One) || Input.GetKey(KeyCode.K) || isPressed)
         {
             voiceExperience.Activate();
-            Debug.Log("Listening from input");
-        }*/
+        }
 
 
 
     }
 
-    /*public void VoiceActivate()
+    public void VoiceActivate()
     {
         voiceExperience.Activate();
-        Debug.Log("Listening from button");
-    }*/
+    }
 
 
 
@@ -52,7 +49,6 @@ public class VoiceScript : MonoBehaviour
     {
         isPressed = false;
         onReleased?.Invoke();
-        voiceExperience.Activate();
         Debug.Log("Button released");
     }
 }
